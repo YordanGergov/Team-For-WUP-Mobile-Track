67ae9d00d2e12eb3c429f538978069b205bae4bf
alexizvely <alexizvely@gmail.com>
2015-12-17 17:10:42 +0200

    Happy HTTP Server App-Skeleton

---
+++ HappyHTTPServer/HappyHTTPServer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace HappyHTTPServer.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged == null)
+            {
+                return;
+            }
+            var propertyEventArgs = new PropertyChangedEventArgs(propertyName);
+            this.PropertyChanged.Invoke(this, propertyEventArgs);
+        }
+    }
+}
