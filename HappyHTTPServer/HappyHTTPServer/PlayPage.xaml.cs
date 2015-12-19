2f634b8d8e9207ffb9f417c0c7a55568d4f4ce64
alexizvely <alexizvely@gmail.com>
2015-12-20 01:42:02 +0200

    test

--- HappyHTTPServer/HappyHTTPServer/PlayPage.xaml.cs
+++ HappyHTTPServer/HappyHTTPServer/PlayPage.xaml.cs
@@ -17,30 +17,38 @@
     using Windows.UI.Xaml.Input;
     using Windows.UI.Xaml.Media;
     using Windows.UI.Xaml.Navigation;
-
+    using Windows.UI.Xaml.Shapes;
     public sealed partial class PlayPage : Page
     {
         private Accelerometer accelerometer;
         private TokenServerViewModel happyServerVM;
+        private FieldViewModel field;
+        private List<Ellipse> fieldEllipses;
 
 
         public PlayPage()
         {
             this.InitializeComponent();
+            this.field = new FieldViewModel();
+            field.Height = this.Field.ActualHeight;
+            field.Width = this.Field.ActualWidth;
+            fieldEllipses = new List<Ellipse>();
+            GenerateFieldObjects();
             this.happyServerVM = new TokenServerViewModel();
             this.DataContext = this.happyServerVM;
             this.accelerometer = Accelerometer.GetDefault();
             this.accelerometer.ReportInterval = 50;
-            this.accelerometer.ReadingChanged += new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(ReadingChanged);           
+            this.accelerometer.ReadingChanged += new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(ReadingChanged);
         }
 
         async private void ReadingChanged(object Accelerometer, AccelerometerReadingChangedEventArgs e)
         {
+           
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 AccelerometerReading reading = e.Reading;
                 var top = Canvas.GetTop(HappyServer);
-                var left = Canvas.GetLeft(HappyServer);                
+                var left = Canvas.GetLeft(HappyServer);
                 var x = reading.AccelerationX;
                 var y = reading.AccelerationY;
                 var z = reading.AccelerationZ;
@@ -67,24 +75,69 @@
                 {
                     newLeft -= speed * (pitchAngle - 90);
                 }
-                
+
                 this.happyServerVM.TopSize = newTop + this.HappyServer.Height;
                 this.happyServerVM.LeftSize = newLeft + this.HappyServer.Width;
 
                 //// the following is to stop our object not to go outside of the canvas
-                if (this.happyServerVM.TopSize > this.HappyServer.Height && this.happyServerVM.TopSize < this.Field.ActualHeight)
+                if (this.happyServerVM.TopSize > this.HappyServer.Height && this.happyServerVM.TopSize < this.field.Height)
                 {
                     Canvas.SetTop(HappyServer, newTop);
                 }
 
-                if (this.happyServerVM.LeftSize > this.HappyServer.Width && this.happyServerVM.LeftSize < this.Field.ActualWidth)
+                if (this.happyServerVM.LeftSize > this.HappyServer.Width && this.happyServerVM.LeftSize < this.field.Width)
                 {
                     Canvas.SetLeft(HappyServer, newLeft);
                 }
 
                 this.DataContext = this.happyServerVM;
-                
+
             });
+        }
+
+        private void GenerateFieldObjects()
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = 25;
+            ellipse.Height = 25;
+            var countTop = 10;
+            var countLeft = 10;
+
+            //// upper ellipses
+            for (int i = 0; i < field.CountObjectsInWidth; i++)
+            {
+                this.Field.Children.Add(ellipse);
+                Canvas.SetTop(ellipse, 0);
+                Canvas.SetLeft(ellipse, countLeft + i * 25);
+                fieldEllipses.Add(ellipse);
+            }
+
+            //// left ellipses
+            for (int i = 0; i < field.CountObjectsInWidth; i++)
+            {
+                this.Field.Children.Add(ellipse);
+                Canvas.SetTop(ellipse, countTop + i * 25);
+                Canvas.SetLeft(ellipse, 0);
+                fieldEllipses.Add(ellipse);
+            }
+
+            //// bottom ellipses
+            for (int i = 0; i < field.CountObjectsInWidth; i++)
+            {
+                this.Field.Children.Add(ellipse);
+                Canvas.SetTop(ellipse, field.Height - 25);
+                Canvas.SetLeft(ellipse, countLeft + i * 25);
+                fieldEllipses.Add(ellipse);
+            }
+
+            //// right ellipses
+            for (int i = 0; i < field.CountObjectsInWidth; i++)
+            {
+                this.Field.Children.Add(ellipse);
+                Canvas.SetTop(ellipse, countTop + i * 25);
+                Canvas.SetLeft(ellipse, field.Width - 25);
+                fieldEllipses.Add(ellipse);
+            }
         }
     }
 }
---
+++ HappyHTTPServer/HappyHTTPServer/ViewModels/FieldViewModel.cs
@@ -0,0 +1,77 @@
+﻿namespace HappyHTTPServer.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FieldViewModel : BaseViewModel
+    {
+        private double width;
+
+        private double height;
+
+        private int countObjectsInWidth;
+
+        private int countObjectsInHeight;
+
+        public FieldViewModel()
+        {
+            this.CountObjectsInHeight = (int)this.Height / 25 - 1;
+            this.CountObjectsInWidth = (int)this.Width / 25 - 1;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+                this.OnPropertyChanged("Width");
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+                this.OnPropertyChanged("Height");
+            }
+        }
+
+        public int CountObjectsInWidth
+        {
+            get
+            {
+                return this.countObjectsInWidth;
+            }
+            set
+            {
+                this.countObjectsInWidth = value;
+                this.OnPropertyChanged("CountObjectsInWidth");
+            }
+        }
+
+        public int CountObjectsInHeight
+        {
+            get
+            {
+                return this.countObjectsInHeight;
+            }
+            set
+            {
+                this.countObjectsInHeight = value;
+                this.OnPropertyChanged("CountObjectsInHeight");
+            }
+        }
+    }
+}
