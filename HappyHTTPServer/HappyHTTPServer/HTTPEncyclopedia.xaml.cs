a9b55665caecfda660c1a4b3f7699b7885c61118
YordanGergov <yordan.gergov91@gmail.com>
2015-12-20 14:24:07 +0200

    logo changes; started working on a real tutorial
    it's not pretty

--- HappyHTTPServer/HappyHTTPServer/HTTPEncyclopedia.xaml.cs
+++ HappyHTTPServer/HappyHTTPServer/HTTPEncyclopedia.xaml.cs
@@ -27,22 +27,16 @@ namespace HappyHTTPServer
 
         private async void RunButtonClick(object sender, RoutedEventArgs e)
         {
-            // Setup the configuration to support document loading
-            var config = Configuration.Default.WithDefaultLoader();
+             var config = Configuration.Default.WithDefaultLoader();
 
-            // Load the names of all The Big Bang Theory episodes from Wikipedia
             var address = "https://en.wikipedia.org/wiki/List_of_HTTP_status_codes";
 
-            // Asynchronously get the document in a new context using the configuration
             var document = await BrowsingContext.New(config).OpenAsync(address);
 
-            // This CSS selector gets the desired content
             var cellSelector = "dl dt";
-
-            // Perform the query to get all cells with the content
+            
             var cells = document.QuerySelectorAll(cellSelector);
 
-            // We are only interested in the text - select it with LINQ
             var titles = cells.Select(m => m.TextContent);
 
             this.ResultTextBlock.Text = string.Join(Environment.NewLine, titles);
