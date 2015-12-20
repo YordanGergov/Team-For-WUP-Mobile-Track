9a14a6e9294e4853aee4e1122db058c898faa1ab
alexizvely <alexizvely@gmail.com>
2015-12-20 02:55:40 +0200

    customview

---
+++ HappyHTTPServer/HappyHTTPServer/CustomViews/CanvasListView.cs
@@ -0,0 +1,34 @@
+﻿namespace HappyHTTPServer.CustomViews
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Data;
+
+    public class CanvasListView : ListView
+    {
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            var control = element as FrameworkElement;
+            if (control == null)
+            {
+                return;
+            }
+
+            var topBinding = new Binding()
+            {
+                Path = new PropertyPath("Top")
+            };
+
+            control.SetBinding(Canvas.TopProperty, topBinding);
+
+            var leftBinding = new Binding()
+            {
+                Path = new PropertyPath("Left")
+            };
+
+            control.SetBinding(Canvas.LeftProperty, leftBinding);
+
+            base.PrepareContainerForItemOverride(element, item);
+        }
+    }
+}
